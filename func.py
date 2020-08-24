62d6bc6e2d9c8e85bc06d8ec5bf5948217e8d7d0
zymzs <ZhangY.M.0533@gmail.com>
2020-08-24 11:27:00 +0800

    Update func.py

--- func.py
+++ func.py
@@ -1,27 +1,34 @@
+'''
+Title: give-three-point-get-twice-function
+Author: zymzs
+Date: 2020/08/23
+License: GNU Ver.3
+'''
+
 from sympy import *
 
 while True:
     
-    # defind
+    # define
     a = symbols('a')
     b = symbols('b')
     c = symbols('c')
     
     
     # get coordinate of points as temp for further format
-    p1t = input("Please enter Point 1(format like '(1,1)' without ','):")
-    p2t = input("Please enter Point 2(format like '(1,1)' without ','):")
-    p3t = input("Please enter Point 3(format like '(1,1)' without ','):")
+    p1t = input("Please enter Point 1(format like '(1,1)'):")
+    p2t = input("Please enter Point 2(format like '(1,1)'):")
+    p3t = input("Please enter Point 3(format like '(1,1)'):")
     
     # format the points
-    p1x = int(p1t.split(',')[0][1:])
-    p1y = int(p1t.split(',')[1][:-1])
+    p1x = float(p1t.split(',')[0][1:])
+    p1y = float(p1t.split(',')[1][:-1])
      # print(p1x)
      # print(p1y)
-    p2x = int(p2t.split(',')[0][1:])
-    p2y = int(p2t.split(',')[1][:-1])
-    p3x = int(p3t.split(',')[0][1:])
-    p3y = int(p3t.split(',')[1][:-1])
+    p2x = float(p2t.split(',')[0][1:])
+    p2y = float(p2t.split(',')[1][:-1])
+    p3x = float(p3t.split(',')[0][1:])
+    p3y = float(p3t.split(',')[1][:-1])
     
     
     # Wrong! For you can only use ** but not ^ in python.
@@ -30,7 +37,6 @@ while True:
     result = solve([Eq(a*(p1x^2)+b*p1x+c, int(p1y)), \
                     Eq(a*(p2x^2)+b*p2x+c, int(p2y)), \
                     Eq(a*(p3x^2)+b*p3x+c, int(p3y))], [a, b, c])
-    #result = solve([a*])
     '''
     
     # calculate
@@ -40,8 +46,8 @@ while True:
         
     print(result)
     
-    aaa = input("Continue? (Y/N):")
-    if aaa == "Y":
+    feedback = input("Continue? (Y/N):")
+    if feedback.lower() == "y":
         continue
     else:
         break
